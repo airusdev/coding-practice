89fd4792048d57c2f22d92a1d1e251957b3ef0bc
airusdev <work.airus@gmail.com>
2026-07-05 19:01:16 +0800

    solved

--- hackerrank/algorithms/warmup/8 minimax_sum.py
+++ hackerrank/algorithms/warmup/8 minimax_sum.py
@@ -12,26 +12,41 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
+# my solution
 def miniMaxSum(arr):
     minimum = min(arr)
     maximum = max(arr)
 
-    min_sum = 0
-    max_sum = 0
+    if minimum == maximum:
+        print(minimum * (len(arr) - 1), minimum * (len(arr) - 1))
+    else:
+        min_sum = 0
+        max_sum = 0
 
-    for num in arr:
-       if num != minimum:
-           max_sum += num
-       if num != maximum:
-           min_sum += num 
+        for num in arr:
+            if num != minimum:
+                max_sum += num
+            if num != maximum:
+                min_sum += num 
 
-    print(min_sum, max_sum)
+        print(min_sum, max_sum)
 
 if __name__ == '__main__':
 
     arr = list(map(int, input().rstrip().split()))
 
     miniMaxSum(arr)
+
+
+# smart solution
+"""
+def miniMaxSum(arr):
+    # Write your code here
+    total = sum(arr)
+    minisum = total - min(arr)
+    maxsum = total - max(arr)
+    print(f'{maxsum} {minisum}')
+"""
 
 
 """
