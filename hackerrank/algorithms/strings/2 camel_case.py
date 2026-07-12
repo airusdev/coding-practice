9a3256c8c9d43ef1cbd19344e7f6ac8d8ab2e73b
airusdev <work.airus@gmail.com>
2026-07-12 18:49:36 +0800

    solved

---
+++ hackerrank/algorithms/strings/2 camel_case.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    word_count = 1
+
+    for letter in s:
+        if letter.isupper():
+            word_count += 1
+
+    return word_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
